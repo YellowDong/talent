c0fdcfed77a48075dfd089502daff3a0b0a76d75
dong <787773580@qq.com>
2019-09-20 18:24:19 +0800

    添加二级路由和相关的视图函数

--- taohua/yunliu/myuser/models.py
+++ taohua/yunliu/myuser/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+	SEX_CHOICE = (
+		('1', '男'),
+		('2', '女')
+	)
+	username = models.CharField(max_length=240, unique=True)
+	sex = models.CharField(max_length=2, default='1', choices=SEX_CHOICE)
+	email = models.EmailField(unique=True)
+	password = models.CharField(max_length=240)
+	c_time = models.DateTimeField(auto_now_add=True)
+
+	class Meta:
+		ordering = ['-c_time']
+		verbose_name_plural = verbose_name = '用户'
+
+	def __str__(self):
+		return self.username
